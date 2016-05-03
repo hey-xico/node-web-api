cc002aab314d214db138c12e71d7a016297eb85f
Chico <francisco.almeida@outloo.com>
2016-05-02 22:27:49 -0300

    adding mongoose suport. Create Cat Schema and update routes to suport mongoose

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ var app = express();
 
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
+
 app.use(bodyParser.json());//Enable bodyParser when the app is a JSON
 app.use(bodyParser.urlencoded({extended: true}));//Parse the body form 
 
-var cats = require('./cats.js')(app);//the cats.js exports a function which takes the app as parameter
+//The app is singleton... to hit this one we need to pass it through
+
+var cats = require('./cat_routes.js')(app);//the cats.js exports a function which takes the app as parameter
 
 var server = app.listen(3000, function() {
 	console.log('Server running at http://127.0.0.1:3000/');
